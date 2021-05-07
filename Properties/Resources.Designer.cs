47b128f275dbbc9b92a576157696a0e9a6adb80a
ReEdix <reedixpro@gmail.com>
2021-05-07 16:15:11 +0200

    Fix jpg error

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -69,15 +69,5 @@ namespace TCPclient.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Mankala1 {
-            get {
-                object obj = ResourceManager.GetObject("Mankala1", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
